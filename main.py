e2ea1645d5ccfafa37e6fdcf66e9fb6f017be5f7
JeroenRaaf <jeroenraaf@hotmail.com>
2024-02-12 17:10:18 +0100

    GET and POST

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+strings = []
+
+
+@app.route('/getArray')
+def hello():
+    return strings
+
+
+@app.route('/addToArray', methods=['POST'])
+def addToArray():
+    data = request.get_json()
+    print(data)
+    strings.append(data['string'])
+    return jsonify({"status": "success"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
